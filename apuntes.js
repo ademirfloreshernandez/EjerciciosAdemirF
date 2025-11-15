ac9f1de7628308c927fa8bde484774370ffa2fe1
Ademir Flores <ademirfloreshernandez@gmail.com>
2025-11-14 21:10:51 -0600

    frutas

--- apuntes.js
+++ apuntes.js
@@ -12,7 +12,7 @@ Function
 */
 
  function DatosArreglos(){
- let datos =[3,7,9,3,2];
+ /* let datos =[3,7,9,3,2,6];
  let suma = 0
  for(i=0;i<datos.length;i++)
     if(datos[i]%2===0){
@@ -20,6 +20,6 @@ Function
 }
 
 console.log(`la suma de los datos es ${suma}`);
-
+ */
 }
 DatosArreglos();
